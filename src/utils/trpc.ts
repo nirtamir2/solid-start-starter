c4795aa14bec06d45009a450034965c7ee671a38
Nir Tamir <nirtamir2@gmail.com>
2022-11-12 14:12:26 +0200

    fix env variables wrong import

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,11 +1,11 @@
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
-import { serverEnv } from "~/env/serverEnv";
 import type { IAppRouter } from "~/server/trpc/router/_app";
 
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return "";
-  if (serverEnv.VERCEL_URL != null) return `https://${serverEnv.VERCEL_URL}`;
-  return `http://localhost:${serverEnv.PORT}`;
+  if (process.env["VERCEL_URL"] != null)
+    return `https://${process.env["VERCEL_URL"]}`;
+  return `http://localhost:${process.env["PORT"] ?? 3000}`;
 };
 
 export const client = createTRPCProxyClient<IAppRouter>({
