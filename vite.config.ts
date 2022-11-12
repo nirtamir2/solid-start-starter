c3c32e0e8a158270a4a28c2c35257d1ef9988f65
Nir Tamir <nirtamir2@gmail.com>
2022-11-12 21:55:58 +0200

    fix vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -13,12 +13,13 @@ export default defineConfig(() => {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-call
       devtools({ name: true }),
       solid({
+        ssr: false,
         // TODO: set island flags for better performance when they will work well with signals
         // islands: true,
         // islandsRouter: true,
 
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment,@typescript-eslint/no-unsafe-call
-        adapter: vercel(),
+        adapter: vercel({ edge: false }),
       }),
     ],
   };
