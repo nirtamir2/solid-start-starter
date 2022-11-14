c86372985c192a541b6918c0f7634f42d03fd019
Nir Tamir <nirtamir2@gmail.com>
2022-11-14 19:47:29 +0200

    locator feature of solid-devtools

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,4 +1,9 @@
 import "solid-devtools";
+import { useLocator } from "solid-devtools";
 import { StartClient, mount } from "solid-start/entry-client";
+
+useLocator({
+  targetIDE: "webstorm",
+});
 
 mount(() => <StartClient />, document);
