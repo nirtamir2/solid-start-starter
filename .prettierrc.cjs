0e3f1f450b6bf7690334cd4b59979430b615b826
Nir Tamir <nirtamir2@gmail.com>
2022-11-25 23:55:20 +0200

    upgrade dependencies and remove hack to fix prettier config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,32 +1,10 @@
-//#region pluginToMakeTailwindAndSortImportsWorkTogether
-const pluginSortImports = require("@trivago/prettier-plugin-sort-imports");
-const pluginTailwindcss = require("prettier-plugin-tailwindcss");
-
-/** @type {import("prettier").Parser}  */
-const typescriptParser = {
-  ...pluginSortImports.parsers.typescript,
-  parse: pluginTailwindcss.parsers.typescript.parse,
-};
-
-/** @type {import("prettier").Plugin}  */
-const pluginToMakeTailwindAndSortImportsWorkTogether = {
-  parsers: {
-    typescript: typescriptParser,
-  },
-};
-//#endregion
-
 module.exports = {
   plugins: [
-    /**
-     * This is a workaround for making tailwind and sort-imports plugins work together
-     * @See https://github.com/tailwindlabs/prettier-plugin-tailwindcss/issues/31#issuecomment-1195411734
-     */
-    pluginToMakeTailwindAndSortImportsWorkTogether,
-    require.resolve("prettier-plugin-tailwindcss"),
-    require.resolve("@trivago/prettier-plugin-sort-imports"),
     require.resolve("prettier-plugin-packagejson"),
+    require.resolve("@trivago/prettier-plugin-sort-imports"),
+    require.resolve("prettier-plugin-tailwindcss"), // must come last
   ],
+  pluginSearchDirs: false, // for prettier-plugin-tailwindcss to work with other plugins
   // @see https://github.com/tailwindlabs/prettier-plugin-tailwindcss#resolving-your-tailwind-configuration
   tailwindConfig: "./tailwind.config.cjs",
   // @see https://github.com/trivago/prettier-plugin-sort-imports
