a8cbff8b596d53e50361850a4d1f1748aba8e144
Nir Tamir <nirtamir2@gmail.com>
2022-12-30 22:42:25 +0200

    routes-gen - patch solid-start

--- driver.cjs
+++ driver.cjs
@@ -1,36 +1,22 @@
 const path = require("path");
 const { exec } = require("child_process");
-const appDirectory = ".";
-const defaultOutputPath = `${appDirectory}/src/routes.d.ts`;
+const appDirectory = "./src";
+const defaultOutputPath = `${appDirectory}/routes.d.ts`;
 const watchPaths = async () => [
   `${appDirectory}/routes/**/*.{ts,tsx,js,jsx}`,
 ];
-const parseRoutes = (routes, parentPath) => routes
-    .map((item) => {
-      var _a;
-      const path = `${parentPath !== null && parentPath !== void 0 ? parentPath : ""}${(_a = item.path) !== null && _a !== void 0 ? _a : ""}`;
-      return [
-        {
-          path: (path.endsWith("/") && path !== "/"
-              ? path.slice(0, -1)
-              : path).replace(/\/\/+/g, "/"),
-        },
-        ...(item.children ? parseRoutes(item.children, `${path}/`) : []),
-      ];
-    })
-    .flat()
-    .filter((route, index, routes) => routes.findIndex((comparedRoute) => comparedRoute.path === route.path) === index)
-    .filter((item) => Boolean(item.path) && !item.path.includes("/*"));
 const routes = async () => {
   const scriptPath = path.join(__dirname, 'node_modules', 'solid-start', 'bin.cjs');
   const parsedRoutes = await new Promise(async (resolve) => {
     exec(`node ${scriptPath} routes`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
+      if (error != null) {
+        console.error("error", error);
         return;
       }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
+      if(stderr != null){
+        console.error("stderr", stderr);
+        return;
+      }
       const routes = JSON.parse(stdout);
       resolve(routes);
     });
