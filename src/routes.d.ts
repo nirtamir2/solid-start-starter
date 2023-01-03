78b20aaa302457a5ff2df620e4866343c58c981f
Nir Tamir <nirtamir2@gmail.com>
2023-01-04 00:02:00 +0200

    generate some routes for testing

--- src/routes.d.ts
+++ src/routes.d.ts
@@ -1,7 +1,16 @@
 declare module "routes-gen" {
   export type RouteParams = {
+    "/:param": { "param": string };
     "/": Record<string, never>;
+    "/api/cool": Record<string, never>;
+    "/api/cool/:param": { "param": string };
   };
 
-  export function route<T extends ["/"]>(...args: T): typeof args[0];
+  export function route<
+    T extends
+      | ["/:param", RouteParams["/:param"]]
+      | ["/"]
+      | ["/api/cool"]
+      | ["/api/cool/:param", RouteParams["/api/cool/:param"]]
+  >(...args: T): typeof args[0];
 }
---
+++ src/routes/api/cool/[param].tsx
@@ -0,0 +1,40 @@
+import { A } from "@solidjs/router";
+import { Suspense } from "solid-js";
+import type { RouteDataArgs } from "solid-start";
+import { createRouteData, useRouteData } from "solid-start";
+import GithubIcon from "~/assets/github.svg";
+import SolidIcon from "~/assets/solid.svg";
+import { Counter } from "~/components/Counter";
+import { client } from "~/utils/trpc";
+
+export function routeData(_: RouteDataArgs) {
+  return createRouteData(async () => {
+    return await client.hello.query({ name: "World" });
+  });
+}
+
+export default function Home() {
+  const greeting = useRouteData<typeof routeData>();
+
+  return (
+    <div class="flex h-screen flex-col items-center justify-center gap-4">
+      <h1 class="text-center text-4xl font-bold text-gray-900">
+        Solid Start Starter
+      </h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div class="text-center text-2xl font-bold text-gray-500">
+          {greeting()}
+        </div>
+      </Suspense>
+      <SolidIcon height={200} width={200} />
+      <Counter />
+      <A
+        href="https://github.com/nirtamir2/solid-start-starter"
+        class="flex items-center gap-2 text-gray-500"
+      >
+        <GithubIcon height={24} width={24} />
+        <span>solid-start-starter</span>
+      </A>
+    </div>
+  );
+}
